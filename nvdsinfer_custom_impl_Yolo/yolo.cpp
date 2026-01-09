ee890b2297512339d30b76ecd7225debb2ac14be
Ernest Lim <ernestlwt@users.noreply.github.com>
2026-01-09 16:02:09 +0800

    copied change from main repo pr #677

--- nvdsinfer_custom_impl_Yolo/yolo.cpp
+++ nvdsinfer_custom_impl_Yolo/yolo.cpp
@@ -202,12 +202,27 @@ Yolo::createEngine(nvinfer1::IBuilder* builder)
   assert(runtime);
 
 #if NV_TENSORRT_MAJOR >= 8
-    nvinfer1::IHostMemory* serializedEngine = builder->buildSerializedNetwork(*network, *config);
-    nvinfer1::ICudaEngine* engine = runtime->deserializeCudaEngine(serializedEngine->data(), serializedEngine->size());
-    delete serializedEngine;
+  nvinfer1::IHostMemory* serializedEngine = builder->buildSerializedNetwork(*network, *config);
+
+  nvinfer1::ICudaEngine* engine = nullptr;
+  if (serializedEngine) {
+    engine = runtime->deserializeCudaEngine(serializedEngine->data(), serializedEngine->size());
+  }
 #else
-    // Fallback for TensorRT 7 (JetPack 4.5.1)
-    nvinfer1::ICudaEngine* engine = builder->buildEngineWithConfig(*network, *config);
+  nvinfer1::ICudaEngine* engine = builder->buildEngineWithConfig(*network, *config);
+#endif
+
+  if (engine) {
+    std::cout << "Building complete\n" << std::endl;
+  }
+  else {
+    std::cerr << "Building engine failed\n" << std::endl;
+  }
+
+#if NV_TENSORRT_MAJOR >= 8
+  if (serializedEngine) {
+    delete serializedEngine;
+  }
 #endif
 
 #ifdef GRAPH
